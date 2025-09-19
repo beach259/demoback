26f180d27d33bd4facf5c173c282edbd70a6e9b2
kulumi <beach1251427652@gmail.com>
2025-09-19 15:40:23 +0800

    feat: 初始化前端项目基础结构和SQL数据库表
    添加Vue.js前端项目基础文件结构，包括配置文件、入口文件和组件
    创建学生、管理员和教职工的SQL数据库表结构

---
+++ SQL/demosql.sql
@@ -0,0 +1,36 @@
+-- 学生表
+DROP TABLE IF EXISTS student;
+CREATE TABLE student (
+    student_id VARCHAR(20) PRIMARY KEY COMMENT '学生ID',
+    studentname VARCHAR(10) UNIQUE NOT NULL COMMENT '学生姓名',
+    password VARCHAR(255) NOT NULL COMMENT '密码',
+    phone VARCHAR(20) COMMENT '手机号',
+    email VARCHAR(100) COMMENT '邮箱',
+    address VARCHAR(255) COMMENT '家庭住址',
+    age INT COMMENT '年龄'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='学生基本信息表';
+
+INSERT INTO student (student_id, studentname, password, phone, email, address, age)
+VALUES ('S2025001', '张三', '123456', NULL, NULL ,NULL , NULL);
+
+
+-- 管理员表
+DROP TABLE IF EXISTS admin;
+CREATE TABLE admin (
+    admin_id VARCHAR(20) PRIMARY KEY COMMENT '管理员ID',
+    password VARCHAR(255) NOT NULL COMMENT '密码'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='管理员表';
+
+INSERT INTO admin (admin_id, password)
+VALUES ('A001', '123456');
+
+
+-- 教职工/迎新工作人员表
+DROP TABLE IF EXISTS staff;
+CREATE TABLE staff (
+    staff_id VARCHAR(20) PRIMARY KEY COMMENT '工作人员ID',
+    password VARCHAR(255) NOT NULL COMMENT '密码'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='工作人员表';
+
+INSERT INTO staff (staff_id, password)
+VALUES ('T001', 'staff123');
